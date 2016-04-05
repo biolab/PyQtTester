2fa3027cfd29f7b58a937d307a254af98ed3dbe0
Kernc <kerncece@gmail.com>
2016-04-05 03:47:58 +0200

    Move files into a separate module so x11_subprocess.sh installs
    Fixes https://github.com/biolab/PyQtTester/issues/2

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from pyqttester import __version__
 
+VERSION = '0.1.0'
 
 if __name__ == '__main__':
     setup(
         name="PyQtTester",
         description="Test Python Qt (PyQt) applications",
-        version=__version__,
+        version=VERSION,
         author='Bioinformatics Laboratory, FRI UL',
-        author_email='contact@orange.biolab.si',
+        author_email='info@biolab.si',
         url='https://github.com/biolab/PyQtTester',
         keywords=(
             'PyQt', 'testing', 'TDD'
         ),
         packages=find_packages(),
-        py_modules=(
-            'pyqttester',
-        ),
-        package_data={
-        },
+        include_package_data=True,
         install_requires=(
         ),
         entry_points={
